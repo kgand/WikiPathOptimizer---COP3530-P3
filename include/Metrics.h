67ca00987b70d959fe2f67b4842d83f5c911a8b2
kgand <34491606+kgand@users.noreply.github.com>
2024-11-10 00:04:01 -0500

    Add SearchMetrics struct for algorithm comparison

--- include/Metrics.h
+++ include/Metrics.h
@@ -1,15 +1,12 @@
 #ifndef METRICS_H
 #define METRICS_H
 
-#include <chrono>
-
+// struct to store metrics for comparing search algorithms
 struct SearchMetrics {
-    int pathLength;
-    double executionTime;
-    size_t memoryUsed;
-    bool pathFound;
-    
-    SearchMetrics() : pathLength(0), executionTime(0.0), memoryUsed(0), pathFound(false) {}
+    bool pathFound = false; // whether a path was found
+    double executionTime = 0.0; // time taken in milliseconds
+    size_t pathLength = 0; // length of path found
+    size_t nodesVisited = 0; // number of nodes visited during search
 };
 
 #endif // METRICS_H 
